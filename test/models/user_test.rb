2c6d4d73e15a81f3f06ebcb0d6b3a83ce197844d
Cami Hayes <camihayes72@gmail.com>
2018-04-20 17:12:18 +0000

    add user and recipe validations

---
+++ test/models/user_test.rb
@@ -0,0 +1,63 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  
+  def setup
+    @user = User.new(username: "camille", email: "camille@example.com")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.username = " "
+    assert_not @user.valid?
+  end
+  
+  test "name should be less than 30 characters" do
+    @user.username = "a" * 31
+    assert_not @user.valid?
+  end
+  
+  test "email should be present" do
+    @user.email = " "
+    assert_not @user.valid?
+  end
+  
+  test "email should not be too long" do
+    @user.email = "a" * 245 + "@example.com"
+    assert_not @user.valid?
+  end
+  
+  test "email should accept correct format" do
+    valid_emails = %w[user@example.com MASHRUR@gmail.com M.first@yahoo.ca john+smith@co.uk.org]
+    valid_emails.each do |valids|
+      @user.email = valids
+      assert @user.valid?, "#{valids.inspect} should be valid"
+    end
+  end
+  
+  test "should reject invalid addresses" do
+    invalid_emails = %w[mashrur@example mashrur@example,com mashrur.name@gmail. joe@bar+foo.com]
+    invalid_emails.each do |invalids|
+      @user.email = invalids
+      assert_not @user.valid?, "#{invalids.inspect} should be invalid"
+    end
+  end 
+  
+  test "email should be unique and case insensitive" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  
+  test "email should be lower case before hitting db" do
+    mixed_email = "JOHN@example.com"
+    @user.email = mixed_email
+    @user.save
+    assert_equal mixed_email.downcase, @user.reload.email
+  end
+  
+end
