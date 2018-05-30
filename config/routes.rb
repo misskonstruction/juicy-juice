c98c4ce2117938c52962baa3d17b65c397262f92
Cami Hayes <camihayes72@gmail.com>
2018-05-30 15:10:47 +0000

    completed alpha phase

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
    
    resources :recipes do
     resources :comments, only: [:create]
+    member do 
+    post 'like'
   end
+end
    
    get '/signup', to: 'users#new'
    resources :users, except: [:new]
@@ -19,4 +22,5 @@ Rails.application.routes.draw do
    get '/chat', to: 'chatrooms#show'
    
    resources :messages, only: [:create]
+   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_23_034542) do
+ActiveRecord::Schema.define(version: 2018_05_29_184047) do
 
   create_table "comments", force: :cascade do |t|
     t.text "description"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2018_05_23_034542) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.boolean "like"
+    t.integer "user_id"
+    t.integer "recipe_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "messages", force: :cascade do |t|
@@ -42,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_05_23_034542) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image"
   end
 
   create_table "users", force: :cascade do |t|
