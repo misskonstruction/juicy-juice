7a790c916e4b85e0b47dee67eb3fe09ab86fe270
Cami Hayes <camihayes72@gmail.com>
2018-06-01 20:21:26 +0000

    increased size of recipes allowed

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :name, presence: true
-  validates :description, presence: true, length: {minimum: 5, maximum: 500}
+  validates :description, presence: true, length: {minimum: 5, maximum: 5000}
   default_scope -> {order(updated_at: :desc)}
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
